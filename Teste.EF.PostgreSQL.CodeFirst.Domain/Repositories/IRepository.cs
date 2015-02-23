806b23716ae9b03194e465ea7dafb53779faebb4
michelgomessilva <michel.gomes.silva@gmail.com>
2015-02-23 18:10:45 -0300

    Versão Alpha

---
+++ Teste.EF.PostgreSQL.CodeFirst.Domain/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.EF.PostgreSQL.CodeFirst.Domain.Repositories
+{
+    public interface IRepository<T>
+    {
+        T GetByKey(Guid key);
+        IList<T> GetList(T entity);
+        void Add(T entity);
+        void Edit(T entity);
+        void Remove(T entity);
+        
+    }
+}
---
+++ Teste.EF.PostgreSQL.CodeFirst.Infra/ProductRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Teste.EF.PostgreSQL.CodeFirst.Domain.Entities;
+using Teste.EF.PostgreSQL.CodeFirst.Domain.Repositories;
+
+namespace Teste.EF.PostgreSQL.CodeFirst.Infra
+{
+    public class ProductRepository : IRepository<Product>
+    {
+        public Product GetByKey(Guid key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Product> GetList(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
